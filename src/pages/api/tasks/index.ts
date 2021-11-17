74e2e19d6a121d619efbd74074a4b382bffbeb2a
T0w3r <torres.alejo2001@gmail.com>
2021-11-17 13:45:12 -0300

    try catch for tasks route

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,21 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { conn } from "../../../utils/database";
 export default async function task(req: NextApiRequest, res: NextApiResponse) {
-  const { method,body } = req;
+  const { method, body } = req;
   switch (method) {
     case "GET":
-        return res.status(200).json("getting task");
-
+      try {
+        const query = `SELECT * FROM tasks`;
+        const response = await conn.query(query);
+        return res.status(200).json(response.rows);
+      } catch (error:any) {
+        res.status(500).json({ error: error.message });
+      }
     case "POST":
+      try {
         const { title, description } = body;
 
-        const query = "INSERT INTO tasks(title,description) VALUES($1,$2)"
-        const values = [title,description];
-        const response = await conn.query(query,values);
-        console.log(response)
-        return res.status(200).json("creating task");
+        const query =
+          "INSERT INTO tasks(title,description) VALUES($1,$2) RETURNING *";
+        const values = [title, description];
+        const response = await conn.query(query, values);
 
+        return res.status(200).json(response.rows[0]);
+      } catch (error:any) {
+        return res.status(500).json({ error: error.message });
+      }
     default:
-        return res.status(400).json("method not allowed");
+      return res.status(400).json("method not allowed");
   }
 }
